c964ff274e0eeec06ba49c2c8db5069f8799d141
Van Hien <vhien1501@gmail.com>
2016-08-15 16:42:48 +0700

    last commit
    final

---
+++ WebApplication1/WebApplication1/DeliveryChangeItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace WebApplication1
+{
+    public class DeliveryChangeItem
+    {
+        public int ID;
+        public string JobNo;
+        public DateTime NewDeliveryDate;
+        public string ReasonForChange;
+        public string Comments;
+        public string CustomerName;
+        public DateTime OriginalDeliveryDate;
+        public int UpdatedBy;
+        public DateTime Created;
+        public string UserLogin;
+        public string UserName;
+        
+    }
+}
--- WebApplication1/WebApplication1/Report.aspx.cs
+++ WebApplication1/WebApplication1/Report.aspx.cs
@@ -14,32 +14,33 @@ namespace WebApplication1
     public partial class Report : System.Web.UI.Page
     {
         static string connectionString = "Data Source=hq03; Initial Catalog=EmplasPortal; User ID='dbdev'; Password='Hqgaming123'";
-        public static List<myObject> ListItem() {
-            List<myObject> options = new List<myObject>();
+        public static List<DeliveryChangeItem> ListItem() {
+            List<DeliveryChangeItem> options = new List<DeliveryChangeItem>();
             using (SqlConnection connection = new SqlConnection(connectionString)) {
                 connection.Open();
                 string from = DateTime.Today.ToString("yyyy/MM/dd HH:mm:ss");
                 string to = DateTime.Today.AddDays(1).AddSeconds(-1).ToString("yyyy/MM/dd HH:mm:ss");
-                string sql = "SELECT id,job_no,new_delivery_date,reason_for_change,comments,customer_name,original_delivery_date,updated_by,created,user_login,user_name FROM ft_deliverychanges tbA LEFT JOIN ft_users tbB ON tbA.updated_by=tbB.user_id WHERE tbA.created between '"+from+"' and '"+to+"'";
-                Console.WriteLine(from + to);
+                string sql = "SELECT id,job_no,new_delivery_date,reason_for_change,comments,customer_name,original_delivery_date,updated_by,created,user_login,user_name FROM ft_deliverychanges tbA LEFT JOIN ft_users tbB ON tbA.updated_by=tbB.user_id WHERE tbA.created between @from and @to";
                 SqlCommand cmd = new SqlCommand(sql, connection);
+                cmd.Parameters.AddWithValue("@from", DateTime.Today);
+                cmd.Parameters.AddWithValue("@to", DateTime.Today.AddDays(1).AddSeconds(-1));
                 using (SqlDataReader reader = cmd.ExecuteReader()) {
                     if (reader.HasRows)
                     {
                         while (reader.Read())
                         {
-                            myObject item = new myObject();
-                            item.Id =Convert.ToInt32(reader["id"]);
-                            item.Job_no = reader["job_no"].ToString();
-                            item.New_delivery_date = Convert.ToDateTime(reader["new_delivery_date"]);
-                            item.Reason_for_change = reader["reason_for_change"].ToString();
+                            DeliveryChangeItem item = new DeliveryChangeItem();
+                            item.ID =Convert.ToInt32(reader["id"]);
+                            item.JobNo = reader["job_no"].ToString();
+                            item.NewDeliveryDate = Convert.ToDateTime(reader["new_delivery_date"]);
+                            item.ReasonForChange = reader["reason_for_change"].ToString();
                             item.Comments = reader["comments"].ToString();
-                            item.Customer_name = reader["customer_name"].ToString();
-                            item.Original_delivery_date = Convert.ToDateTime(reader["original_delivery_date"]);
-                            item.Updated_by = Convert.ToInt32(reader["updated_by"]);
+                            item.CustomerName = reader["customer_name"].ToString();
+                            item.OriginalDeliveryDate = Convert.ToDateTime(reader["original_delivery_date"]);
+                            item.UpdatedBy = Convert.ToInt32(reader["updated_by"]);
                             item.Created = Convert.ToDateTime(reader["created"]);
-                            item.User_login = reader["user_login"].ToString();
-                            item.User_name = reader["user_name"].ToString();
+                            item.UserLogin = reader["user_login"].ToString();
+                            item.UserName = reader["user_name"].ToString();
                             options.Add(item);
                         }
                     }
@@ -50,7 +51,7 @@ namespace WebApplication1
         }
 
         private void printItem() {
-            List<myObject> itemList = new List<myObject>();
+            List<DeliveryChangeItem> itemList = new List<DeliveryChangeItem>();
             itemList = ListItem();
             int number = 0;
             if (itemList.Count == 0)
@@ -61,20 +62,20 @@ namespace WebApplication1
                 tableResults.Controls.Add(tr);
             }
             else {
-                foreach (myObject obj in itemList) {
+                foreach (DeliveryChangeItem obj in itemList) {
                     number++;
                     HtmlGenericControl tr = new HtmlGenericControl("tr");
                     tr.InnerHtml += "<td class=\"col-sm-1 text-right\">"+number+"</td>";
-                    tr.InnerHtml += "<td class=\"col-sm-1 text-left\">" + obj.Job_no + "</td>";
-                    tr.InnerHtml += "<td class=\"col-sm-1 text-left\">" + obj.Original_delivery_date.ToString("dd/MM/yyyy") + "</td>";
-                    tr.InnerHtml += "<td class=\"col-sm-1 text-left\">" + obj.New_delivery_date.ToString("dd/MM/yyyy") + "</td>";
-                    tr.InnerHtml += "<td class=\"col-sm-1 text-left\">" + obj.Reason_for_change+ "</td>";
+                    tr.InnerHtml += "<td class=\"col-sm-1 text-left\">" + obj.JobNo + "</td>";
+                    tr.InnerHtml += "<td class=\"col-sm-1 text-left\">" + obj.OriginalDeliveryDate.ToString("dd/MM/yyyy") + "</td>";
+                    tr.InnerHtml += "<td class=\"col-sm-1 text-left\">" + obj.NewDeliveryDate.ToString("dd/MM/yyyy") + "</td>";
+                    tr.InnerHtml += "<td class=\"col-sm-1 text-left\">" + obj.ReasonForChange+ "</td>";
                     tr.InnerHtml += "<td class=\"col-sm-2 text-left\">" + obj.Comments + "</td>";
-                    tr.InnerHtml += "<td class=\"col-sm-3 text-left\">" + obj.Customer_name + "</td>";
+                    tr.InnerHtml += "<td class=\"col-sm-3 text-left\">" + obj.CustomerName + "</td>";
                     //tr.InnerHtml += "<td style=\"text-align:center;\">" + obj.Updated_by + "</td>";
                     tr.InnerHtml += "<td class=\"col-sm-1 text-left\">" + obj.Created.ToString("dd/MM/yyyy") + "</td>";
                     //tr.InnerHtml += "<td style=\"text-align:center;\">" + obj.User_login + "</td>";
-                    tr.InnerHtml += "<td class=\"col-sm-1 text-left\">" + obj.User_name + "</td>";
+                    tr.InnerHtml += "<td class=\"col-sm-1 text-left\">" + obj.UserName + "</td>";
                     tableResults.Controls.Add(tr);
                 }
             }
