4366127de621506f699a62a11311546da14a3e81
Van Hien <vhien1501@gmail.com>
2016-08-15 15:48:28 +0700

    report_delivery_change
    last change

---
+++ WebApplication1/WebApplication1/Report.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Report.aspx.cs" Inherits="WebApplication1.Report" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link href="/assets/css/bootstrap.min.css" rel="stylesheet" media="screen" />
+    <link href="/assets/css/bootstrap-theme.min.css" rel="stylesheet" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div class="table-responsive">
+        <table class="table table-striped table-bordered table-hover table-condensed" border="1">
+            <thead>
+                <tr>
+                    <th style="text-align:center">ID</th>
+                    <th style="text-align:center">Job No</th>
+                    <th style="text-align:center">Original Delivery Date</th>
+                    <th style="text-align:center">New Delivery Date</th>
+                    <th style="text-align:center">Reason For Change</th>
+                    <th style="text-align:center">Comments</th>
+                    <th style="text-align:center">Customer Name</th>
+                    <%--<th>updated_by</th>--%>
+                    <th style="text-align:center">Created</th>
+                    <%--<th>user_login</th>--%>
+                    <th style="text-align:center">User Name</th>
+                </tr>
+            </thead>
+            <tbody id="tableResults" runat="server">
+            </tbody>
+        </table>
+    </div>
+    </form>
+    <script src="/assets/js/jquery-2.1.1.js"></script>
+    <script src="/assets/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ WebApplication1/WebApplication1/Report.aspx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+
+//水木 Nguyen
+
+namespace WebApplication1
+{
+    public partial class Report : System.Web.UI.Page
+    {
+        static string connectionString = "Data Source=hq03; Initial Catalog=EmplasPortal; User ID='dbdev'; Password='Hqgaming123'";
+        public static List<myObject> ListItem() {
+            List<myObject> options = new List<myObject>();
+            using (SqlConnection connection = new SqlConnection(connectionString)) {
+                connection.Open();
+                string from = DateTime.Today.ToString("yyyy/MM/dd HH:mm:ss");
+                string to = DateTime.Today.AddDays(1).AddSeconds(-1).ToString("yyyy/MM/dd HH:mm:ss");
+                string sql = "SELECT id,job_no,new_delivery_date,reason_for_change,comments,customer_name,original_delivery_date,updated_by,created,user_login,user_name FROM ft_deliverychanges tbA LEFT JOIN ft_users tbB ON tbA.updated_by=tbB.user_id WHERE tbA.created between '"+from+"' and '"+to+"'";
+                Console.WriteLine(from + to);
+                SqlCommand cmd = new SqlCommand(sql, connection);
+                using (SqlDataReader reader = cmd.ExecuteReader()) {
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            myObject item = new myObject();
+                            item.Id =Convert.ToInt32(reader["id"]);
+                            item.Job_no = reader["job_no"].ToString();
+                            item.New_delivery_date = Convert.ToDateTime(reader["new_delivery_date"]);
+                            item.Reason_for_change = reader["reason_for_change"].ToString();
+                            item.Comments = reader["comments"].ToString();
+                            item.Customer_name = reader["customer_name"].ToString();
+                            item.Original_delivery_date = Convert.ToDateTime(reader["original_delivery_date"]);
+                            item.Updated_by = Convert.ToInt32(reader["updated_by"]);
+                            item.Created = Convert.ToDateTime(reader["created"]);
+                            item.User_login = reader["user_login"].ToString();
+                            item.User_name = reader["user_name"].ToString();
+                            options.Add(item);
+                        }
+                    }
+                }
+                
+            }
+            return options;
+        }
+
+        private void printItem() {
+            List<myObject> itemList = new List<myObject>();
+            itemList = ListItem();
+            int number = 0;
+            if (itemList.Count == 0)
+            {
+                HtmlGenericControl tr = new HtmlGenericControl("tr");
+                
+                tr.InnerHtml += "<td colspan=\"12\" style=\"text-align:center;\">No result found</td>";
+                tableResults.Controls.Add(tr);
+            }
+            else {
+                foreach (myObject obj in itemList) {
+                    number++;
+                    HtmlGenericControl tr = new HtmlGenericControl("tr");
+                    tr.InnerHtml += "<td class=\"col-sm-1 text-right\">"+number+"</td>";
+                    tr.InnerHtml += "<td class=\"col-sm-1 text-left\">" + obj.Job_no + "</td>";
+                    tr.InnerHtml += "<td class=\"col-sm-1 text-left\">" + obj.Original_delivery_date.ToString("dd/MM/yyyy") + "</td>";
+                    tr.InnerHtml += "<td class=\"col-sm-1 text-left\">" + obj.New_delivery_date.ToString("dd/MM/yyyy") + "</td>";
+                    tr.InnerHtml += "<td class=\"col-sm-1 text-left\">" + obj.Reason_for_change+ "</td>";
+                    tr.InnerHtml += "<td class=\"col-sm-2 text-left\">" + obj.Comments + "</td>";
+                    tr.InnerHtml += "<td class=\"col-sm-3 text-left\">" + obj.Customer_name + "</td>";
+                    //tr.InnerHtml += "<td style=\"text-align:center;\">" + obj.Updated_by + "</td>";
+                    tr.InnerHtml += "<td class=\"col-sm-1 text-left\">" + obj.Created.ToString("dd/MM/yyyy") + "</td>";
+                    //tr.InnerHtml += "<td style=\"text-align:center;\">" + obj.User_login + "</td>";
+                    tr.InnerHtml += "<td class=\"col-sm-1 text-left\">" + obj.User_name + "</td>";
+                    tableResults.Controls.Add(tr);
+                }
+            }
+
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            printItem();
+        }
+    }
+}
